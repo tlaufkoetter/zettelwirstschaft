32588a64776c09a749a86ddd2c7104cbd55bad1a
Tobias Laufkötter <tobias.laufkoetter@gmail.com>
2021-05-01 19:00:02 +0200

    Factor out setup and teardown logic.

--- ZettelWirtschaft.Application.Test/ValueObject/ValueObjectTests.cs
+++ ZettelWirtschaft.Application.Test/ValueObject/ValueObjectTests.cs
@@ -6,58 +6,20 @@ using ZettelWirtschaft.Application.ValueObject;
 
 namespace ZettelWirtschaft.Application.Test.ValueObject
 {
-    public class ValueObjectTests : IDisposable
+    public class ValueObjectTests : AbstractFixture
     {
         private static bool calledValidate;
         private static ValidationResult validationResult;
 
-        private void SetUp()
+        override protected void SetUp()
         {
             calledValidate = false;
             validationResult = null;
         }
 
-        private void TearDown()
+        override protected void TearDown()
         {
         }
-
-        #region Dispose
-        private bool disposedValue;
-
-        public void CallsValidationInConstructorTest()
-        {
-            SetUp();
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    TearDown();
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
-                disposedValue = true;
-            }
-        }
-
-        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        // ~ValueObjectTests()
-        // {
-        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        //     Dispose(disposing: false);
-        // }
-
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
-        #endregion
 
         private class ValueObjectMockValidator<TValue> : AbstractValidator<TValue>
         {
