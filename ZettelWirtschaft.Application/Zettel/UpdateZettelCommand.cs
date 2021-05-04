abf6207dcfa8bc1c9da0edd5ecf8214b7af740d1
Tobias Laufkötter <tobias.laufkoetter@gmail.com>
2021-05-04 11:58:10 +0200

    Add udpate Zettel logic.

---
+++ ZettelWirtschaft.Application/Zettel/UpdateZettelCommand.cs
@@ -0,0 +1,48 @@
+using System.Security.Cryptography;
+using System.Threading;
+using System;
+using System.Collections.Generic;
+using MediatR;
+using System.Threading.Tasks;
+
+namespace ZettelWirtschaft.Application.Zettel
+{
+    public class UpdateZettelCommand : IRequest<ZettelEntity>
+    {
+        private ZettelEntity zettel;
+        public ZettelEntity Zettel { get => zettel; set => zettel = value ?? throw new ArgumentNullException(nameof(Zettel)); }
+
+        public UpdateZettelCommand(ZettelEntity zettel)
+        {
+            Zettel = zettel;
+        }
+    }
+
+    public interface IUpdateZettelRepository
+    {
+        Task<ZettelEntity> UpdateZettel(ZettelEntity zettel, CancellationToken cancellation);
+        Task<bool> DoesZettelExist(ZettelId zettelId1, CancellationToken cancellation);
+    }
+
+    public class UpdateZettelCommandHandler : IRequestHandler<UpdateZettelCommand, ZettelEntity>
+    {
+        private readonly IUpdateZettelRepository updateRepository;
+
+        public UpdateZettelCommandHandler(IUpdateZettelRepository updateRepository)
+        {
+            this.updateRepository = updateRepository;
+        }
+
+        public async Task<ZettelEntity> Handle(UpdateZettelCommand request, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            if (!(await updateRepository.DoesZettelExist(request.Zettel.Id, cancellationToken)))
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                throw new ArgumentException($"Zettel with id {request.Zettel.Id} does not exist");
+            }
+
+            return await updateRepository.UpdateZettel(request.Zettel, cancellationToken);
+        }
+    }
+}
